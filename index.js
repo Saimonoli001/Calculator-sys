0724ea171e72edf89c801ab0c7fa54083be53fe6
Saimonoli001 <saimonoli781@gmail.com>
2025-12-02 19:56:47 +0545

    Fix syntax errors and improve formatting in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const display = document.getElementById("display");
 function appendToDisplay(input){
     display.value += input;
 }
-
+}
 function clearDisplay(){
     display.value = "";
 }
@@ -15,4 +15,6 @@ function calculate(){
     catch(error){
         display.value = "Error";
     }
+}
+}
 }
